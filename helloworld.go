54fdde77fc530ba9b9e3b044925ea0c767393073
MaiXiaochai <maixiaochai@outlook.com>
2021-03-02 21:03:34 +0800

    *)字符串操作

--- helloworld.go
+++ helloworld.go
@@ -1,6 +1,9 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+)
 
 const (
 	a1, a2 = iota + 1, iota + 2
@@ -8,8 +11,34 @@ const (
 )
 
 func main() {
-	fmt.Println("a1: ", a1)
-	fmt.Println("a2: ", a2)
-	fmt.Println("a3: ", a3)
-	fmt.Println("a4: ", a4)
+	s1 := "D:\\data\\maixiaochai"
+	s2 := "Hi, I'm Tom."
+	s3 := `D:/data/maixiaochai
+			Hi, I'm Tom.`
+	fmt.Println(s1)
+	fmt.Println(s2)
+	fmt.Println(s3)
+	//
+	//fmt.Println(s1 + s2)
+	//result := fmt.Sprintf("%s%s", s1, s2)
+	//fmt.Println(result)
+
+	spl := strings.Split(s1, "\\")
+	fmt.Println(spl)
+	//fmt.Println("a1: ", a1)
+	//fmt.Println("a2: ", a2)
+	//fmt.Println("a3: ", a3)
+	//fmt.Println("a4: ", a4)
+
+	s4 := "Hello汤姆"
+	fmt.Println(len(s4))
+
+	//for i := 0; i < len(s4); i++ {
+	//	//fmt.Println(s4[i])
+	//	//fmt.Printf("%c\n", s4[i])
+	//}
+
+	for _, c := range s4 {
+		fmt.Printf("%c\n", c)
+	}
 }
