51deec72a1bb74ddb229383b93cf8ff9d6524d38
MaiXaochai <maixiaochai@outlook.com>
2021-03-11 16:18:16 +0800

    feat:添加字 defer经典理解用题

--- deferClassicCase.go
+++ deferClassicCase.go
@@ -1,33 +1,39 @@
 package main
 
+// defer 经典理解题目
+// 含有defer和return的函数的执行过程
+// 	1. 返回值=x
+// 	2. 运行defer
+// 	3. RET指令
+
 func f1() int {
 	x := 5
 	defer func() {
-		x++ // 修改的是x，不是返回值
+		x++ // 2. x = 6； 3.修改的是x，不是返回值，返回值=5
 	}()
-	return x // 5
+	return x // 1. 返回值 = x = 5
 }
 
-func f2() (x int) {
+func f2() (x int) { // 1. 返回值 = x, 这个时候 返回值只是申请了内存，并没有放值
 	defer func() {
-		x++
+		x++ // 2. x = x + 1; x = 5，则 x = x + 1 = 6; 返回值 = x = 6
 	}()
 	return 5
 }
 
-func f3() (y int) {
+func f3() (y int) { // 1. 返回值 = y
 	x := 5
 	defer func() {
-		x++
+		x++ // 2.这里改变的是x，而 返回值已经申请了内存，并赋值，不影响 返回值； 3.返回值 = 5
 	}()
-	return x
+	return x // 1.2 返回值 = y = x = 5
 }
 
-func f4() (x int) {
+func f4() (x int) { // 1.返回值 = x,申请了内存，未赋值
 	defer func(x int) {
-		x++
+		x++ // 2.这里的x是func内部自己的x，因为是传入的参数；3. 返回值=5
 	}(x)
-	return 5
+	return 5 // 1.2 返回值 = x = 5，内存上赋了值
 }
 
 func main() {
