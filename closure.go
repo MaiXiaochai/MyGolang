bdc330e9a567463d239e0dcbb61593dc4a784ed7
MaiXaochai <maixiaochai@outlook.com>
2021-03-15 15:05:59 +0800

    feat:添加闭包函数

--- closure.go
+++ closure.go
@@ -3,16 +3,25 @@ package main
 import "fmt"
 
 // 闭包
-func f1(f func()) {
-	fmt.Println("I'm f1")
+// 应用场景：将函数fc2作为函数传给fc1，但是不能修改函数fc1
+func fc1(f func()) {
+	fmt.Println("I'm fc1")
 	f()
 }
 
-func f2(x, y int) {
-	fmt.Println("I'm f2")
+func fc2(x, y int) {
+	fmt.Println("I'm fc2")
 	fmt.Println(x + y)
 }
 
-func main() {
+// 解决方法：将fc2写成闭包，返回一个复合fc1参数要求的函数
+func fc3(f func(int, int), x, y int) func() {
+	return func() {
+		f(x, y)
+	}
+}
 
+func main() {
+	ret := fc3(fc2, 10, 12)
+	fc1(ret)
 }
